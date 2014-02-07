5a454729d928e47718d2277cbe6121e9d3e64481
Ra1nWarden <wzh19921016@gmail.com>
2014-02-06 20:58:50 -0500

    & now works!

--- gash.rs
+++ gash.rs
@@ -41,7 +41,13 @@ impl Shell {
 	    self.cmd_history.push(cmd_line.clone());
             let program = cmd_line.splitn(' ', 1).nth(0).expect("no program");
             
-            match program {
+	    let mut mod_prog = program.clone();
+	    let prog_length = mod_prog.len();
+	    if mod_prog.slice_from(prog_length - 1) == "&" {
+	       mod_prog = mod_prog.slice_to(prog_length - 1);
+	    }	    
+
+            match mod_prog {
                 ""      =>  { continue; }
                 "exit"  =>  { return; }
                 _       =>  { self.run_cmdline(cmd_line); }
@@ -49,13 +55,51 @@ impl Shell {
         }
     }
     
-    fn run_cmdline(&mut self, cmd_line: &str) {
+    fn run_cmdline(&mut self, cmd_line: &str) {	 
         let mut argv: ~[~str] =
             cmd_line.split(' ').filter_map(|x| if x != "" { Some(x.to_owned()) } else { None }).to_owned_vec();
-    
         if argv.len() > 0 {
-            let program: ~str = argv.remove(0);
-            self.run_cmd(program, argv);
+	   let program: ~str = argv.remove(0);
+	   let mut mod_prog = program.clone();
+	   let mut background = false;
+	   if argv.len() == 0 {
+	      let prog_length = mod_prog.len();    
+	      if mod_prog.slice_from(prog_length - 1) == "&" {
+	      	 mod_prog = mod_prog.slice_to(prog_length - 1).trim().to_owned();
+	       	 background = true;
+	      }
+	   }
+	   else {
+	      let mut last_arg = argv[argv.len() - 1].clone();
+	      if last_arg == ~"&" {
+	      	 argv.pop();
+		 background = true;
+	      }
+	      else {
+	      	   let arg_length = last_arg.len();
+	      	   if last_arg.slice_from(arg_length - 1) == "&" {
+	      	      last_arg = last_arg.slice_to(arg_length - 1).to_owned();
+		      argv.pop();
+		      argv.push(last_arg);
+		      background = true;
+	      	   } 
+	      }  
+	   }
+	   if background {
+	      let prog = mod_prog.clone();
+	      let arguments = argv.clone();
+	      if self.cmd_exists(prog) {
+	      	 do spawn {
+		    run::process_status(prog, arguments);
+		 }
+	      }
+	      else {
+	      	 println!("{:s}: command not found", program);
+	      }
+	   }
+	   else {
+	   	self.run_cmd(program, argv);
+	   }
         }
     }
     
@@ -74,12 +118,12 @@ impl Shell {
 	       }
 	    }
 	    else {
-              run::process_status(program, argv);
+            	 run::process_status(program, argv);
             }
         } 
 	else if program == "history" {
 	     for i in range(0, self.cmd_history.len()) {
-	     	 println!("{:u} {:s}", i+1, self.cmd_history[i]);
+	     	 println!(" {:u} {:s}", i+1, self.cmd_history[i]);
 	     }
 	}
 	else {
